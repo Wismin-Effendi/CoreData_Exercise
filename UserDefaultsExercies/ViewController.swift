bbb94414449a10b500ec04499f3d6b35cbfd4c35
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-19 15:25:39 -0500

    Implement saving user fields into UserDefaults.

--- UserDefaultsExercies/ViewController.swift
+++ UserDefaultsExercies/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var genderField: UITextField!
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var locationField: UITextField!
+    @IBOutlet weak var quoteTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let userDefaults = UserDefaults.standard
+        nameField.text = userDefaults.string(forKey: "Name")
+        genderField.text = userDefaults.string(forKey: "Gender")
+        ageField.text = userDefaults.string(forKey: "Age")
+        locationField.text = userDefaults.string(forKey: "Location")
+        quoteTextView.text = userDefaults.string(forKey: "Quote")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(nameField.text, forKey: "Name")
+        userDefaults.set(genderField.text, forKey: "Gender")
+        userDefaults.set(ageField.text, forKey: "Age")
+        userDefaults.set(locationField.text, forKey: "Location")
+        userDefaults.set(quoteTextView.text, forKey: "Quote")
     }
 
 
